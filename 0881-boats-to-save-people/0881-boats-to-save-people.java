7b061b7e310a816a729879b0d1aa1abc1fc861d0
Paras Jain <paras726770@gmail.com>
2026-07-18 16:25:46 +0530

    Time: 19 ms (98.5%), Space: 56.2 MB (89.96%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int ans=0;
+
+        int i=0, j=people.length-1;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                ans++;
+                i++;
+                j--;
+            }
+            else{
+                ans++;
+                j--;
+            }
+        }
+        return ans;
+    }
+}
