8158c80efec916a1b2fa822ace2fca4bf4a3399a
Paras Jain <paras726770@gmail.com>
2026-09-02 01:12:49 +0530

    Time: 320 ms (84.4%), Space: 0B (100%) - LeetHub

---
+++ 3657-find-loyal-customers/3657-find-loyal-customers.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+Select customer_id 
+from customer_transactions 
+group by customer_id
+having sum(transaction_type = 'purchase') > 2
+and
+datediff(max(transaction_date),min(transaction_date))>29
+and
+sum(transaction_type ='refund')/count(*)<0.2
+order by 1;
